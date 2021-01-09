5c527adc26ba7be4dd5052351b42f6bb65765287
ksh168 <mailkunalsharma99@gmail.com>
2021-01-09 12:03:02 +0000

    file validation and others added

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,37 +1,61 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-
 from flaskblog.models import User
 
+
 class RegistrationForm(FlaskForm):
-	#setting up username(it's a string, required field, length from 2 to 20 characters)
-	username = StringField('Username', 
-					validators=[DataRequired(), Length(min=2, max=20)])
-	email = StringField('Email',
-						validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm Password',
-							 validators=[DataRequired(), EqualTo('password')])
-	submit = SubmitField('Sign Up')
+    #setting up username(it's a string, required field, length from 2 to 20 characters)
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
-	def validate_username(self, username):
-		user = User.query.filter_by(username=username.data).first()
-		#raise error if username already exists
-		if user:
-			raise ValidationError('Username already exists')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        #raise error if username already exists
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
 
-	def validate_email(self, email):
-		user = User.query.filter_by(email=email.data).first()
-		#raise error if email already exists
-		if user:
-			raise ValidationError('Email already registered')
-
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        #raise error if email already exists
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
-	email = StringField('Email',
-						validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	remember = BooleanField('Remember Me')
-	submit = SubmitField('Login')
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+#to let users update their details
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            #raise error if username already exists
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            #raise error if email already exists
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
